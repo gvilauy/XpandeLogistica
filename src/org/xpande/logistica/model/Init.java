c48bb81094feafff194e264eff58784a863b8727
gabriel <gabrielvila13@gmail.com>
2020-08-05 09:44:38 -0300

    Initial commit

---
+++ src/org/xpande/logistica/model/Init.java
@@ -0,0 +1,10 @@
+package org.xpande.logistica.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 8/5/20.
+ */
+public class Init {
+
+
+}
